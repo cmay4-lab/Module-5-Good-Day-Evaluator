ae4ae5f9fe37095aa1fb2f8cd702703a13c3efd7
cmay4-lab <cmay4@bucs.fsw.edu>
2024-10-16 16:39:04 -0400

    Version 2, I wrote some outlining of the Main class, but could not do more because of technical difficulties with login.

---
+++ Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+    // https://en.wikipedia.org/wiki/Dew_point
+    // Calculating the dew point
+    // The dew-point temperature is a relation between the actual "dry bulb" temperature (T) and the relative humidity (RH)
+    // Florida's Yearly average humidity is 75%
+    // Florida's Yearly average temperature is 73 degrees F (rounded)
+    // I will make these constants so I wont have to factor in locational/psychological factors.
+
+    private static void isWeatherGood() {
+
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+
+        System.out.print("\n\n");    // Prints new lines to distinguish where the console's preconfigurations end and where the main method begins.
+
+        // The main idea behind this next section is that the tests simulates an imaginary user using the Good Day
+        // Evaluator. This is the 1st user, and this is their 1st day using it. Since their GoodDay attributes are
+        // extreme, they are likely having a bad day...
+
+        GoodDay goodDay = new GoodDay();    // Instantiates a blank GoodDay object to test the default constructor.
+
+
+
+
+
+    }
+
+}
